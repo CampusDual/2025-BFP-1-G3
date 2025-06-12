1e8f55dd36a58207a0d3beca5ff2f9872b842ebe
NotOppi <Filippofg@outlook.com>
2025-06-12 09:21:15 +0200

    Display Offers added!

--- Front/src/app/main/display-offers/display-offers.component.ts
+++ Front/src/app/main/display-offers/display-offers.component.ts
@@ -9,9 +9,9 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class DisplayOffersComponent implements OnInit {
 
-  offers!: Offer[];
-  displayedColumns: string[] = ['id', 'title', 'description', 'compant_id'];
-
+  offers: Offer[] = [];
+  displayedColumns: string[] = ['id', 'title', 'offerDescription', 'companyId'];
+  
   constructor(private loginService: LoginService) { }
 
   ngOnInit() {
@@ -19,9 +19,20 @@ export class DisplayOffersComponent implements OnInit {
   }
   
   load(): void {
-    this.loginService.getOffers().subscribe(
-      getOffers => this.offers = getOffers
-    );
-  }
-
+  console.log('Iniciando carga de ofertas...');
+  this.loginService.getOffers().subscribe(
+    getOffers => {
+      this.offers = getOffers;
+      console.log('Ofertas cargadas:', this.offers);
+      console.log('Cantidad de ofertas:', this.offers.length);
+      if (this.offers.length > 0) {
+        console.log('Primera oferta:', this.offers[0]);
+      }
+    },
+    error => {
+      console.error('Error al cargar ofertas:', error);
+      // Mostrar un mensaje al usuario
+    }
+  );
+}
 }
